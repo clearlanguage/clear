fe49be3e9c677fd0e92526281837f1072025a15b
Alan Fayz <alanfayz123@gmail.com>
2024-09-15 00:04:33 +0100

    i give up i am going to bed

---
+++ Source/Source.cpp
@@ -0,0 +1,14 @@
+﻿// alkhat.cpp : Defines the entry point for the application.
+//
+
+#include "alkhat.h"
+
+#include <llvm/IR/IRBuilder.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
