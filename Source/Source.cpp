66bb12a11796e30f1d15b980377d6f4d7f893ad9
Alan Fayz <alan.fayz@hotmail.com>
2025-05-27 15:29:41 +0100

    cdynamicarray

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -19,5 +19,10 @@ int main()
 
     BuildConfig config = {"Tests/new_test.cl"};
     linker.Build(config);
+
+    std::vector<int> a;
+    std::vector<int> b;
+    a.insert(a.begin(), b.begin(), b.end());
+
     return 0;
 }
