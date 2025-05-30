01609f6d357159374b02aab33e0be913a3ee546d
KisAwesome <cool676rock@gmail.com>
2025-05-30 23:40:38 +0400

    Math lib

---
+++ Standard/math.cl
@@ -0,0 +1,59 @@
+import "math.h" as "_cmath"
+
+function sin(float64 x) -> float64:
+    return _cmath.sin(x)
+
+function cos(float64 x) -> float64:
+    return _cmath.cos(x)
+
+function tan(float64 x) -> float64:
+    return _cmath.tan(x)
+
+function asin(float64 x) -> float64:
+    return _cmath.asin(x)
+
+function acos(float64 x) -> float64:
+    return _cmath.acos(x)
+
+function atan(float64 x) -> float64:
+    return _cmath.atan(x)
+
+function atan2(float64 y, float64 x) -> float64:
+    return _cmath.atan2(y, x)
+
+function sinh(float64 x) -> float64:
+    return _cmath.sinh(x)
+
+function cosh(float64 x) -> float64:
+    return _cmath.cosh(x)
+
+function tanh(float64 x) -> float64:
+    return _cmath.tanh(x)
+
+function exp(float64 x) -> float64:
+    return _cmath.exp(x)
+
+function log(float64 x) -> float64:
+    return _cmath.log(x)
+
+function log10(float64 x) -> float64:
+    return _cmath.log10(x)
+
+function pow(float64 base, float64 exponent) -> float64:
+    return _cmath.pow(base, exponent)
+
+function sqrt(float64 x) -> float64:
+    return _cmath.sqrt(x)
+
+function ceil(float64 x) -> float64:
+    return _cmath.ceil(x)
+
+function floor(float64 x) -> float64:
+    return _cmath.floor(x)
+
+function abs(float64 x) -> float64:
+    return _cmath.fabs(x)
+
+
+function logb(float64 x, float64 base) -> float64:
+    return log(x) / log(base)
---
+++ Standard/math.h
@@ -0,0 +1,83 @@
+//
+// Created by Kareem Fares on 5/30/25.
+//
+
+double sin(double);
+double cos(double);
+double tan(double);
+double asin(double);
+double acos(double);
+double atan(double);
+double atan2(double, double);
+double sinh(double);
+double cosh(double);
+double tanh(double);
+double exp(double);
+double log(double);
+double log10(double);
+double pow(double, double);
+double sqrt(double);
+double cbrt(double);
+double ceil(double);
+double floor(double);
+double fabs(double);
+double fmod(double, double);
+double frexp(double, int*);
+double ldexp(double, int);
+double modf(double, double*);
+double round(double);
+double trunc(double);
+
+/* Single precision (float) */
+float sinf(float);
+float cosf(float);
+float tanf(float);
+float asinf(float);
+float acosf(float);
+float atanf(float);
+float atan2f(float, float);
+float sinhf(float);
+float coshf(float);
+float tanhf(float);
+float expf(float);
+float logf(float);
+float log10f(float);
+float powf(float, float);
+float sqrtf(float);
+float cbrtf(float);
+float ceilf(float);
+float floorf(float);
+float fabsf(float);
+float fmodf(float, float);
+float frexpf(float, int*);
+float ldexpf(float, int);
+float modff(float, float*);
+float roundf(float);
+float truncf(float);
+
+/* Long double (optional, platform-dependent) */
+long double sinl(long double);
+long double cosl(long double);
+long double tanl(long double);
+long double asinl(long double);
+long double acosl(long double);
+long double atanl(long double);
+long double atan2l(long double, long double);
+long double sinhl(long double);
+long double coshl(long double);
+long double tanhl(long double);
+long double expl(long double);
+long double logl(long double);
+long double log10l(long double);
+long double powl(long double, long double);
+long double sqrtl(long double);
+long double cbrtl(long double);
+long double ceill(long double);
+long double floorl(long double);
+long double fabsl(long double);
+long double fmodl(long double, long double);
+long double frexpl(long double, int*);
+long double ldexpl(long double, int);
+long double modfl(long double, long double*);
+long double roundl(long double);
+long double truncl(long double);
